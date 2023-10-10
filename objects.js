f14ef4e4050ca8e55ad4a37f04d4ca4f8cc55d48
Nathan Chen <113192742+seribical@users.noreply.github.com>
2023-10-10 13:17:13 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -15,164 +15,37 @@ console.log(gguh.number()); */
 
 const array = [
     {
-        firstname:'a',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
+        firstname:'ehtan dejesus',
+        birthyear:2008,
+        numbers: [1,2,3,4],
     },
     {
-        firstname:'b',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
+        firstname:'reelan',
+        birthyear:2009,
+        numbers: [1,2,3,4],
     },
     {
-        firstname:'c',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
+        firstname:'tristian',
+        birthyear:2007,
+        numbers: [1,2,3,4],
     },
     {
-        firstname:'d',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
+        firstname:'gaberials',
+        birthyear:2010,
+        numbers: [1,2,3,4],
     },
     {
-        firstname:'e',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'f',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'g',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'h',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'i',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'j',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'k',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'l',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'m',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'n',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'o',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'p',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'q',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'r',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'s',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'t',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'u',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'v',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'w',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'x',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'y',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
-    },
-    {
-        firstname:'z',
-        other1:'saaaasad',
-        other2:'saaaasad',
-        other3:'saaaasad',
+        firstname:'noahr',
+        birthyear:2011,
+        numbers: [1,2,3,4],
     },
 ]
 
-array.forEach((name)=> console.log(name.firstname))
-const array2 = array.filter(()=>array.firstname);
-
-console.log(array2)
+const array1 = array.filter((year1)=> year1);
+const array2 = array1.filter((year2)=> year2.birthyear > 2008);
+array2.forEach((birth)=> {
+    console.log(birth.firstname);});
+Object.values(array2).forEach(val => {
+    console.log(val.firstname);
+    console.log(val.birthyear);
+});
